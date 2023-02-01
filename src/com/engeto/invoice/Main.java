7864f8a61903bbd3f4a6ab7f0d7b8529938c8e28
Martin Šimůnek <public@e-mis.cz>
2023-02-01 21:02:02 +0100

    Initial commit

---
+++ src/com/engeto/invoice/Main.java
@@ -0,0 +1,18 @@
+package com.engeto.invoice;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        String item;    // text (posloupnost znaků)
+        int quantity;   // celá čísla
+        // Integer quantity; // celé číslo - objektové
+        double weight;  // desetinná čísla
+        BigDecimal pricePerPiece;
+            // desetinná čísla zpracovávaná
+            // v desítkové soustavě
+        LocalDate date;
+        boolean isDiscouted;    // true × false
+    }
+}
